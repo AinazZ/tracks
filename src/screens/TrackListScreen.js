401ef8f65871150ca07089ac033be16dc144d022
AinazZ <ziyazova.aynaz@mail.ru>
2021-02-05 15:07:48 +0300

    add screens

---
+++ src/screens/TrackListScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const TrackListScreen = () => {
+    return <Text style={{ fontSize: 48 }}>TrackList Screen</Text>;
+};
+
+const styles = StyleSheet.create({});
+
+export default TrackListScreen;
